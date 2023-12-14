f5eb135b846436b72cd79e0edf7f582d0f63e22d
chrismartin5 <chrismartinunc@gmail.com>
2023-12-14 15:24:54 -0500

    hw20

---
+++ HW19/spare copy.js
@@ -0,0 +1,23 @@
+$(".shopSmall img").mouseover(function() {
+    $(".shopSmall img").css({ width: '245px', height: 'auto'});
+});
+
+$(".shopSmall img").mouseout(function() {
+    $(".shopSmall img").css({ width: '250px', height: 'auto' });
+});
+
+$(".tailTalk img").mouseover(function() {
+    $(".tailTalk img").css({ width: '245px', height: 'auto' });
+});
+
+$(".tailTalk img").mouseout(function() {
+    $(".tailTalk img").css({ width: '250px', height: 'auto' });
+});
+
+$(".ncdsf img").mouseover(function() {
+    $(".ncdsf img").css({ width: '245px', height: 'auto' });
+});
+
+$(".ncdsf img").mouseout(function() {
+    $(".ncdsf img").css({ width: '250px', height: 'auto' });
+});
